ef3281e4c0cf7e47bb900f4e7f9d6588065f97c2
александр <shurik.ka4og@yandex.ru>
2021-10-27 09:12:06 +0300

    fgfg

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function getRandomPositiveInteger (a, b) {
   return Math.floor(result);
 };
 
-function getRandomPositiveFloat (a, b, digits = 1) {
+function getRandomPositiveFloat (a, b, digits) {
   const lower = Math.min(Math.abs(a), Math.abs(b));
   const upper = Math.max(Math.abs(a), Math.abs(b));
   const result = Math.random() * (upper - lower) + lower;
@@ -27,130 +27,103 @@ randomAvatarImage(1, 10)
 
 // Title
 
-const title = 'Квартира';
-
-function randomTitle() {
-  return title
-}
-
-randomTitle (title);
+const randomTitle = () => {
+  const TITLE = ['title','title1','title2','title3','title4', 'title5','title6','title7','title8','title9'];
+  const randomNotice = getRandomPositiveInteger(0,TITLE.length - 1);
+  return TITLE[randomNotice];
+  };
 
 // Address
 
-const address = ['location.lat', 'location.lng'];
-const message = address.join(', ');
-console.log(message);
+const addressLatMin = 35.65000;
+const addressLatMax = 35.70000;
+const addressLngMin = 139.70000;
+const addressLngMax = 139.80000;
+
+const addressLat = getRandomPositiveFloat(addressLatMin, addressLatMax, 5);
+const addressLng = getRandomPositiveFloat(addressLngMin, addressLngMax, 5);
+
+const setAddress = (address) => {
+  const first = addressLat;
+  const second = addressLng;
+  return ${first}, ${second};
+};
+
 
 // Price
 
-const price = 0;
-
-function randomPrice(price) {
-  return Math.random();
-}
-
-randomPrice(price);
+const randomPrice = () => {
+  const randomIndex = getRandomPositiveInteger(5, 25);
+  return randomIndex;
+};
 
 // Type
 
-function randomTypePlaces (a, b) {
-
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
+const randomTypePlaces = () => {
+  const TYPE_PLACES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+  const randomIndex = getRandomPositiveInteger(0, TYPE_PLACES.length - 1);
+  return TYPE_PLACES[randomIndex];
 };
-
-let palace = 'palace';
-let flat = 'flat';
-let house = 'house';
-let bungalow = 'bungalow';
-let hotel = 'hotel';
-
-
-const places = [palace, flat, house, bungalow, hotel];
-
-places[randomTypePlaces(0,4)];
 
 // Rooms
 
-const rooms = 0;
-
-function randomPrice(rooms) {
-  return Math.random();
-}
-
-randomPrice(rooms);
+const randomRooms = () => {
+  const randomIndex = getRandomPositiveInteger(1, 5);
+  return randomIndex;
+};
 
 // Checkin
 
-function randomCheckIn (a, b) {
-
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-let midday = '12:00';
-let oneHours = '13:00';
-let twoHours = '14:00';
-
-
-const time = ['12:00', '13:00', '14:00'];
-
-time[randomCheckIn(0,2)];
+const randomCheckIn = () => {
+  const TIME_IN = ['12:00', '13:00', '14:00'];
+  const randomIndex = getRandomPositiveInteger(0, TIME_IN.length - 1);
+  return TIME_IN[randomIndex];
+  }
 
 // Checkout
 
-function randomCheckOut (a, b) {
-
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-let midday = '12:00';
-let oneHours = '13:00';
-let twoHours = '14:00';
-
-
-const time = ['12:00', '13:00', '14:00'];
-
-time[randomCheckOut(0,2)];
+const randomCheckOut = () => {
+  const TIME_OUT = ['12:00', '13:00', '14:00'];
+  const randomIndex = getRandomPositiveInteger(0, TIME_OUT.length - 1);
+  return TIME_OUT[randomIndex];
+  };
 
 // Features
 
-function randomFeatures (a, b) {
-
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
+const randomFeatures = () => {
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const randomIndex = getRandomPositiveInteger(0, FEATURES.length - 1);
+return FEATURES[randomIndex];
 };
-
-let wifi = 'wifi';
-let dishwasher = 'dishwasher';
-let parking = 'parking';
-let washer = 'washer';
-let elevator = 'elevator';
-let conditioner = 'conditioner';
-
-
-const features = [wifi, dishwasher, parking, washer, elevator, conditioner];
-
-features[randomFeatures(0,5)];
 
 // Description
 
-const description = 'Шикарная квартира';
+const randomDescription = () => {
+  const DESCRIPTION = ['Просторное', 'Чистое', 'Удобное', 'Светлое', 'С ремонтом', 'Без ремонта'];
+  const randomIndex = getRandomPositiveInteger(0, DESCRIPTION.length - 1);
+  return DESCRIPTION[randomIndex];
+  };
 
-function randomDescription() {
-  return description
+// Photos
+
+const randomPhotos = () => {
+const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const randomIndex = getRandomPositiveInteger(0, PHOTOS.length - 1);
+return PHOTOS[randomIndex];
+};
+
+// Location
+
+const locationLatMin = 35.65000;
+const locationLatMax = 35.70000;
+const locationLngMin = 139.70000;
+const locationLngMax = 139.80000;
+
+const randomLocation = () => {
+  const locationLat = getRandomPositiveFloat(locationLatMin, locationLatMax, 5);
+  const locationLng = getRandomPositiveFloat(locationLngMin, locationLngMax, 5);
+  return location = {lat: locationLat, lng: locationLng};
 }
-
-randomDescription (description);
 
 
 const createWizardNotice = () => {
